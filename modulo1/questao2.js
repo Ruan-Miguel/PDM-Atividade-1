506b62d040aa7294dba0f77ed6b7cb2959a4c755
Ruan-Miguel <miguelcoelho148@gmail.com>
2020-10-22 16:26:53 -0200

    adicionar as questoes 1 e 7 do modulo 1

---
+++ modulo1/questao2.js
@@ -0,0 +1,15 @@
+const usuarios = [
+  { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+  { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+  { nome: 'Lucas', idade: 30, empresa: 'Facebook' }
+];
+
+const idades = usuarios.map((usuario) => usuario.idade);
+
+const trabalhamRocketseat = usuarios.filter((usuario) => usuario.empresa === 'Rocketseat' && usuario.idade > 18);
+
+const trabalhaGoogle = usuarios.find((usuario) => usuario.empresa === 'Google');
+
+const usuariosIdadeDobrada = usuarios.map((usuario) => Object.assign(usuario, usuario.idade * 2));
+
+const menor50 = usuariosIdadeDobrada.filter((usuario) => usuario.idade <= 50);
